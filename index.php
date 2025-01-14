d2c9360bcfa3678da9322b124b2b9f426db1c891
softwarejhond <softwarejhond@gmail.com>
2025-01-14 18:34:45 -0500

    add address in card

--- index.php
+++ index.php
@@ -90,6 +90,7 @@ $rol = $infoUsuario['rol'];
     <?php include("footer.php"); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/departamentos_municipios_barrios.js?v=2"></script>
+    
     <script src="js/real-time-inquilino-proximo-retiro.js?v=0.1"></script>
     <script src="js/real-time-update-contadores.js?v=0.1"></script>
     <script>
